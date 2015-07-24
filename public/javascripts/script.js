f156816411854150bdd1309b3eea714c9a67dcbe
Trent Kocurek <t2kocurek@gmail.com>
2015-07-24 07:13:21 -0500

    make it larger

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,11 @@
 $(function() {
-  $('.app-name-container').text($('.app-name').val());
-
   $('.app-name').keyup(function() {
     if ($(this).val().length > 0) {
       $('.app-name-container').text($(this).val());
     } else {
       $('.app-name-container').text('Your Apps N...');
     }
-  })
+  });
+
+  $('.app-name').trigger('keyup');
 });
