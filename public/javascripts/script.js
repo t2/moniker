e7506333d3858b7e0ce38ce399684d6207948fbd
Trent Kocurek <t2kocurek@gmail.com>
2015-07-24 06:55:42 -0500

    save design. allow param name.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,6 @@
 $(function() {
+  $('.app-name-container').text($('.app-name').val());
+
   $('.app-name').keyup(function() {
     if ($(this).val().length > 0) {
       $('.app-name-container').text($(this).val());
